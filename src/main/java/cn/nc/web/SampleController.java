4c4c6c26631cb2983af0c025b5d702e68e081cdd
Kyou <wgoodq@gmail.com>
2017-01-11 22:38:49 +0800

    添加Log4j2,未完成

--- src/main/java/cn/nc/web/SampleController.java
+++ src/main/java/cn/nc/web/SampleController.java
@@ -4,15 +4,19 @@ package cn.nc.web;
  * Created by OuKyou on 2017/1/11.
  */
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SampleController {
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
     @RequestMapping("/")
     String home(Model model) {
+        logger.info("test");
         model.addAttribute("name","WebDemo");
         return "index";
     }
